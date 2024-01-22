a7f2bf01a1bcdba4640c0524b9a61aaf77a80d16
Tatsuhiro Motomatsu <tatsu@Jacob.local>
2024-01-22 14:36:10 +0900

    add: eyecatch下部にpublishedAtを挿入

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,9 +22,10 @@ export default function Home({ blog }) {
     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 justify-items-center gap-y-8 gap-x-4'>
       {blog.map((blog) => (
         <li key={blog.id} className='list-none w-40'>
-          <img src={blog.image.url} alt={blog.title} className='w-60 h-30 rounded'/>
           <Link href={`blog/${blog.id}`}>
-            <p className='text-sm'>{blog.title}</p>
+            <img src={blog.image.url} alt={blog.title} className='w-60 h-30 rounded'/>
+            <p className='text-sm font-black'>{blog.title}</p>
+            <p className='text-sm text-neutral-600'>{blog.publishedAt}</p>
           </Link>
         </li>
       ))}
